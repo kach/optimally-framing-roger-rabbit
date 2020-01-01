aeb1ac942428cb5d16294db7acdbd9a7c15c39cd
kach <hardmath123@gmail.com>
2020-01-01 09:40:08 +0530

    rewrote the kd-tree builder based on the pbrt source (kind of)

--- main.py
+++ main.py
@@ -24,12 +24,18 @@ def rotate_face(f, R):
     ])
 
 def face_area(f):
+    # a sanity check to make sure face rotation works properly!
+    # via Heron's formula
     a = vector_size(f[0] - f[1])
     b = vector_size(f[1] - f[2])
     c = vector_size(f[2] - f[0])
     s = (a + b + c) / 2
 
     return (s * (s - a) * (s - b) * (s - c)).sqrt()
+
+
+
+
 
 plydata = plyfile.PlyData.read('bunny/reconstruction/bun_zipper_res4.ply')
 
@@ -44,7 +50,9 @@ for (face,) in plydata['face'].data:
     c = vertices[face[2]]
     faces.append(torch.stack([a, b, c]))
 
-C_TRAV = 1 / 80 # in PBRT, apparently
+C_TRAV = 1/80   # from PBRT kdtreeaccel.cpp:435
+C_ISECT = 1 # from PBRT kdtreeaccel.cpp:435
+
 minf = float('-inf')
 maxf = float('+inf')
 
@@ -56,10 +64,13 @@ maxf = float('+inf')
 
 
 def bounding_box_area(o, c):
+    # just for fun, 2(ab + bc + cd) = (a + b + c)^2 - (a^2 + b^2 + c^2)
     extent = c - o
     return extent.sum() * extent.sum() - (extent * extent).sum()
 
 def split_bounding_box(bb_o, bb_c, axis, division):
+    assert 0 <= axis
+    assert axis <= 2
     L_o = bb_o
     R_o = torch.cat([bb_o[:axis], division.reshape(1, 1), bb_o[axis + 1:]])
 
@@ -68,63 +79,53 @@ def split_bounding_box(bb_o, bb_c, axis, division):
 
     return L_o, L_c, R_o, R_c
 
-def cost_of_subdivision(V, F, bb_o, bb_c, axis, division):
-    L_tris = []
-    R_tris = []
-    for f in F:
-        if any([f[i][axis].item() < division.item() for i in range(3)]):
-            L_tris.append(f)
-        if any([f[i][axis].item() > division.item() for i in range(3)]):
-            R_tris.append(f)
+def build_tree(o, c, V, F):
+    sa = bounding_box_area(o, c)
 
-    L_o, L_c, R_o, R_c = split_bounding_box(bb_o, bb_c, axis, division)
-    L_area = bounding_box_area(L_o, L_c)
-    R_area = bounding_box_area(R_o, R_c)
+    leaf_cost = C_ISECT * len(V)
 
-    cost = C_TRAV + L_area * len(L_tris) + R_area * len(R_tris)
-    return cost, (L_o, L_c, L_tris), (R_o, R_c, R_tris)
+    best_cost = maxf
+    best_division = None
+    best_axis = None
+    best_index = None
 
-def cost_of_cell(V, F, bb_o, bb_c, axis):
-    new_axis = (axis + 1) % 1
+    for axis in range(3):
+        V.sort(key=lambda v: v[axis][0])
+        for i, v in enumerate(V):
+            division = v[axis]
+            L_o, L_c, R_o, R_c = split_bounding_box(o, c, axis, division)
+            p_L = bounding_box_area(L_o, L_c) / sa
+            p_R = bounding_box_area(R_o, R_c) / sa
+            cost = C_TRAV + C_ISECT * (p_L * i + p_R * (len(V) - i))
+            if cost < best_cost:
+                best_cost = cost
+                best_division = division
+                best_axis = axis
+                best_index = i
 
-    my_cost = bounding_box_area(bb_o, bb_c) * len(F)
+    if best_cost > leaf_cost or len(V) <= 1:
+        return leaf_cost
 
-    best_cost = torch.tensor(maxf)
-    best_L = None
-    best_R = None
-    best_idx = None
-
-#   V.sort(key=lambda v: v[axis].item())
-#   F.sort(key=lambda f: min([f[i][axis].item() for i in range(3)]))
-    for i, v in enumerate(V):
-        division = v[axis]
-        cost, L, R = cost_of_subdivision(V, F, bb_o, bb_c, axis, division)
-        if cost.item() < best_cost.item():
-            best_cost = cost
-            best_L = L
-            best_R = R
-            best_idx = i
-    
-    if best_cost < my_cost:
-        #L_optimized = cost_of_cell(V, L[2], L[0], L[1], new_axis)
-        #R_optimized = cost_of_cell(V, R[2], R[0], R[1], new_axis)
-        #return C_TRAV + L_optimized + R_optimized
-        return best_cost
-    else:
-        return my_cost
-
-
-import random
-faces = random.sample(faces, 100)
+    V.sort(key=lambda v: v[best_axis][0])
+    L_o, L_c, R_o, R_c = split_bounding_box(o, c, best_axis, best_division)
+    cost_L = build_tree(L_o, L_c, V[:best_index], F)
+    cost_R = build_tree(R_o, R_c, V[best_index:], F)
+    p_L = bounding_box_area(L_o, L_c) / sa
+    p_R = bounding_box_area(R_o, R_c) / sa
+    return C_TRAV + C_ISECT * (p_L * cost_L + p_R * cost_R)
 
 with open('hack.pbrt') as f:
     template = f.read()
 
+import random
+
 i = 0
 while True:
     i += 1
+
+    print(R)
     V = [rotate_vertex(v, R) for v in vertices]
-    F = [rotate_face(f, R) for f in faces]
+    F = []#[rotate_face(f, R) for f in faces]
 
     bounding_box_origin = torch.tensor([[maxf], [maxf], [maxf]])
     bounding_box_corner = torch.tensor([[minf], [minf], [minf]])
@@ -132,14 +133,15 @@ while True:
         bounding_box_origin = torch.min(bounding_box_origin, v)
         bounding_box_corner = torch.max(bounding_box_corner, v)
 
-    cost = cost_of_cell(V, F, bounding_box_origin, bounding_box_corner, 0)
+    cost = build_tree(bounding_box_origin, bounding_box_corner, V, F)
     print(cost)
-    print(R)
+
     cost.backward()
-    R = R - R.grad * 0.01
+    R = R - R.grad * 0.001
     R = R.detach()
     R.requires_grad_()
 
+    import os
     with open('out/out-%05d.pbrt' % i, 'w') as f:
         import math
         x = template.replace('ROTATION', '%f %f %f %f' % ( vector_size(R) * 360 / math.pi, R[0].item(), R[1].item(), R[2].item() ))
